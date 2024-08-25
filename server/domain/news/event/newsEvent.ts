adcb14135bd20a0746c2f13e1467162a001aeba9
Sho0226 <s1F102201616@iniad.org>
2024-08-26 00:23:59 +0900

    news apiをたたく

---
+++ server/domain/news/event/newsEvent.ts
@@ -0,0 +1,13 @@
+import { NEWS_KEY } from 'service/envValues';
+
+export async function fetchTopHeadslines(query: string): Promise<any> {
+  try {
+    const pageSize = 5;
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${NEWS_KEY}`,
+    );
+  } catch (error) {
+    console.error('Error while caling News API:', error);
+    throw error;
+  }
+}
