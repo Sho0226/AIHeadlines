f7d21491f6c50e0c243d92edc6dd86e20006ea5a
Sho0226 <s1F102201616@iniad.org>
2024-08-26 13:12:45 +0900

    controllerを追加

--- server/api/news/controller.ts
+++ server/api/news/controller.ts
@@ -1,5 +1,20 @@
+import { fetchTopHeadslines } from 'domain/news/event/newsEvent';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: () => ({ status: 200, body: '' }),
+  post: async ({ body }) => {
+    try {
+      const responceNews = await fetchTopHeadslines(body.query);
+      return {
+        status: 200,
+        body: { response: responceNews },
+      };
+    } catch (error) {
+      console.error('Error in fetchTopHeadslines:', error);
+      return {
+        status: 500,
+        body: { error: 'Internal Server Error' },
+      };
+    }
+  },
 }));
