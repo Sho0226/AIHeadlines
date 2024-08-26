2c898e2ca00f5d0211898e122fe9c5c33cd0075a
Sho0226 <s1F102201616@iniad.org>
2024-08-26 21:33:59 +0900

    apiをたたく部分にjson追加

--- server/domain/news/event/newsEvent.ts
+++ server/domain/news/event/newsEvent.ts
@@ -4,8 +4,10 @@ export async function fetchTopHeadslines(query: string): Promise<any> {
   try {
     const pageSize = 5;
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${NEWS_KEY}`,
+      `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${NEWS_KEY}&q=${encodeURIComponent(query)}`,
     );
+    const data = await response.json();
+    return data.articles;
   } catch (error) {
     console.error('Error while caling News API:', error);
     throw error;
