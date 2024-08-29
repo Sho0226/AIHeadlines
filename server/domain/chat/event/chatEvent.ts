9bb7156d9eed846b597fa9f54b1bdc2b6589d943
Sho0226 <s1F102201616@iniad.org>
2024-08-29 13:51:12 +0900

    openaiのオプション

--- server/domain/chat/event/chatEvent.ts
+++ server/domain/chat/event/chatEvent.ts
@@ -1,12 +1,18 @@
 import { openai } from 'service/openai';
 
-export async function streamChatCompletion(quesiton: string): Promise<string> {
+export async function streamChatCompletion(question: string): Promise<string> {
   let responseText = '';
   try {
     const stream = await openai.chat.completions.create({
       model: 'gpt-4o-mini',
-      messages: [{ role: 'user', content: quesiton }],
+      messages: [
+        { role: 'assistant', content: 'a' },
+        { role: 'user', content: question }, // ユーザーが入力する質問
+      ],
       stream: true,
+      max_tokens: 10,
+      temperature: 0.7,
+      frequency_penalty: 0.5,
     });
 
     for await (const chunk of stream) {
