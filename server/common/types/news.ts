ac5d8b7969f4cb42b80fb3a5f550145054224149
Sho0226 <s1F102201616@iniad.org>
2024-09-21 17:53:12 +0900

    news型を厳選

--- server/common/types/news.ts
+++ server/common/types/news.ts
@@ -4,6 +4,4 @@ export type Article = {
   publishedAt: string; // 記事の公開日（ISO 8601形式の文字列）
   url: string; // 記事のURL（Webページのリンク）
   urlToImage: string; // 記事に関連する画像のURL
-  content: string; // 記事の内容
-  description: string; // 記事の短い説明や要約
 };
