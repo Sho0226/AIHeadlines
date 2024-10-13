a5d8dfa8170ceed1feb059a41ecf87ce7fc61243
Sho0226 <s1F102201616@iniad.org>
2024-10-13 17:26:56 +0900

    fix: useCallback to useNews

--- client/hooks/useNews.tsx
+++ client/hooks/useNews.tsx
@@ -1,5 +1,5 @@
 import type { Article } from 'common/types/news';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { apiClient } from 'utils/apiClient';
 
 export const useNews = ({ query }: { query: string }) => {
@@ -7,25 +7,25 @@ export const useNews = ({ query }: { query: string }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const res = await apiClient.news.$post({
-          body: { query },
-        });
-        console.log('Received response from API:', res.response);
-        const newsArticles: Article[] = Array.isArray(res.response) ? res.response : [res.response];
-        setNews(newsArticles || []);
-      } catch (error) {
-        console.error('Error:', error);
-        setError('An error occurred. Please try again.');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchNews();
+  const fetchNews = useCallback(async () => {
+    try {
+      const res = await apiClient.news.$post({
+        body: { query },
+      });
+      console.log('Received response from API:', res.response);
+      const newsArticles: Article[] = Array.isArray(res.response) ? res.response : [res.response];
+      setNews(newsArticles || []);
+    } catch (error) {
+      console.error('Error:', error);
+      setError('An error occurred. Please try again.');
+    } finally {
+      setLoading(false);
+    }
   }, [query]);
+
+  useEffect(() => {
+    fetchNews();
+  }, [fetchNews]);
 
   return { news, loading, error };
 };
